d8f59cc05c29eee4d31babb65158f54e89c7d94b
EjiroOsiephri <osiephriejiro765@gmail.com>
2024-01-05 21:12:21 +0100

    Add: The secrets to socket.io, perfectly used rooms

--- sockets.js
+++ sockets.js
@@ -1,31 +1,48 @@
 let playerReadyCount = 0;
 
+//The concept of room is just like in free fire where
+//we have to wait for the players to complete
+//50 befoe we can go into BR mode, in this game
+//we are creating a room so that when 2 players join they join thesame room
+//The next two will join the next room and so on.
+// We are using the logic playerReadyCount / 2,because we can't increment . the first playerReadyCount will be zero
+// 0/2 is 0 that means room 0 , the next player will have a player ready count of 1 but when we
+// Math.floor(1/2) we get 0 , so the first 2 will join room 0 and the next two will join room 1 and so on
+
 function listen(io) {
   const pongNameSpace = io.of("/pong");
+
+  let room;
+
   pongNameSpace.on("connection", (socket) => {
     console.log("A user connected", socket.id);
 
+    room = "room" + Math.floor(playerReadyCount / 2);
+
+    socket.join(room);
+
     socket.on("ready", () => {
-      console.log("Player ready", socket.id);
+      console.log("Player ready", socket.id, room);
       playerReadyCount++;
 
       if (playerReadyCount % 2 === 0) {
-        pongNameSpace.emit("startGame", socket.id);
+        pongNameSpace.in(room).emit("startGame", socket.id);
       }
     });
 
     socket.on("paddleMove", (paddleData) => {
-      socket.broadcast.emit("paddleMove", paddleData);
+      socket.to(room).emit("paddleMove", paddleData);
     });
 
     socket.on("ballMove", (ballData) => {
-      socket.broadcast.emit("ballMove", ballData);
+      socket.to(room).emit("ballMove", ballData);
     });
 
     socket.on("disconnect", (reason) => {
       console.log(
         `Socket with id ${socket.id} disconnects cause of this: ${reason} `
       );
+      socket.leave(room);
     });
   });
 }
